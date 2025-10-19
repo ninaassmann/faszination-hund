8c68d04d7164c96a785cc4efea1adbc65eb28096
ninaassmann <hallo@ninaassmann.de>
2025-10-19 12:00:53 +0200

    fix migration problems

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,15 +1,9 @@
-import * as migration_20250909_160945_initial from './20250909_160945_initial';
-import * as migration_20251019_093750 from './20251019_093750';
+import * as migration_20251019_095347 from './20251019_095347';
 
 export const migrations = [
   {
-    up: migration_20250909_160945_initial.up,
-    down: migration_20250909_160945_initial.down,
-    name: '20250909_160945_initial',
-  },
-  {
-    up: migration_20251019_093750.up,
-    down: migration_20251019_093750.down,
-    name: '20251019_093750'
+    up: migration_20251019_095347.up,
+    down: migration_20251019_095347.down,
+    name: '20251019_095347'
   },
 ];
