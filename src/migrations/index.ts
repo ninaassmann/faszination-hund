5833e77a5663fde99a9a9e034ce32424ebda4de5
ninaassmann <hallo@ninaassmann.de>
2025-10-19 11:39:24 +0200

    Add Payload migrations

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,4 +1,5 @@
-import * as migration_20250909_160945_initial from './20250909_160945_initial'
+import * as migration_20250909_160945_initial from './20250909_160945_initial';
+import * as migration_20251019_093750 from './20251019_093750';
 
 export const migrations = [
   {
@@ -6,4 +7,9 @@ export const migrations = [
     down: migration_20250909_160945_initial.down,
     name: '20250909_160945_initial',
   },
-]
+  {
+    up: migration_20251019_093750.up,
+    down: migration_20251019_093750.down,
+    name: '20251019_093750'
+  },
+];
