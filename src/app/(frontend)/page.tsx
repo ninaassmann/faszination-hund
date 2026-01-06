dc54ef551970f00226a4026655c562798c49a392
Nina Aßmann <hallo@ninaassmann.de>
2026-01-06 13:54:04 +0100

    test breeds on frontend

--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -1,16 +1,16 @@
 import { getPayload } from 'payload'
+import type { Media, Dogbreed, Tag } from '@/payload-types'
 import configPromise from '@payload-config'
 
 import './styles.css'
-
-function isPopulated<T>(value: number | T | null | undefined): value is T {
-  return typeof value === 'object' && value !== null
-}
+import Image from 'next/image'
+import { ImageIcon } from 'lucide-react'
+import Link from 'next/link'
 
 export default async function HomePage() {
   const payload = await getPayload({ config: configPromise })
 
-  const breeds = await payload.find({
+  const breeds: { docs: Dogbreed[] } = await payload.find({
     collection: 'dogbreeds',
     depth: 3,
     limit: 12,
@@ -18,19 +18,47 @@ export default async function HomePage() {
   })
 
   return (
-    <section className="container">
-      {breeds.docs.map((breed) => (
-        <div key={breed.id} className="card card-border bg-base-100">
-          <div className="card-body">
-            <h3 className="card-title">{breed.breed}</h3>
-            {isPopulated(breed.fci?.fciGroup) && <>{breed.fci.fciGroup.name}</>} |{' '}
-            {isPopulated(breed.fci?.fciSection) && <>{breed.fci.fciSection.name}</>}
-            <div className="card-actions justify-end">
-              <button className="btn btn-primary">Mehr Infos</button>
+    <section className="container flex flex-wrap gap-6 my-40">
+      {breeds.docs.map((breed) => {
+        const thumbnail = breed.images?.find(
+          (img): img is { media: Media; type: 'thumbnail' } =>
+            img.type === 'thumbnail' && typeof img.media !== 'number',
+        )
+
+        return (
+          <div className="card bg-base-100 w-96 shadow-sm overflow-clip" key={breed.id}>
+            {thumbnail?.media.url ? (
+              <div className="bg-gray-200 w-full h-64 flex items-center justify-center aspect-[3/4]">
+                <Image
+                  src={thumbnail.media.url}
+                  alt={thumbnail.media.alt || 'Bild'}
+                  width={thumbnail.media.width || 400}
+                  height={thumbnail.media.height || 300}
+                />
+              </div>
+            ) : (
+              <div className="bg-gray-200 w-full h-64 flex items-center justify-center aspect-[3/4]">
+                <ImageIcon />
+              </div>
+            )}
+            <div className="card-body justify-between">
+              <h2 className="card-title">{breed.breed}</h2>
+              {breed.general?.tags?.map(
+                (tag): tag is Tag => typeof tag !== 'number', // Type Guard
+              ) &&
+                breed.general.tags
+                  .filter((tag): tag is Tag => typeof tag !== 'number') // nur Tag-Objekte
+                  .map((tag) => <span key={tag.id}>{tag.name}</span>)}
+
+              <div className="card-actions justify-end">
+                <Link href={`/blog/${breed.slug}`} className="btn btn-primary">
+                  Mehr erfahren
+                </Link>
+              </div>
             </div>
           </div>
-        </div>
-      ))}
+        )
+      })}
     </section>
   )
 }
