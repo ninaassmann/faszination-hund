2e9e4f879d82b187f7e8ea1b0b20935f3bde78dd
Nina Aßmann <hallo@ninaassmann.de>
2026-01-11 16:26:52 +0100

    Add accordion Block config

---
+++ src/blocks/Accordion/config.ts
@@ -0,0 +1,47 @@
+import { Block } from 'payload'
+
+export const Accordion: Block = {
+  slug: 'accordion',
+  interfaceName: 'Accordion',
+  labels: {
+    singular: 'Akkordeon',
+    plural: 'Akkordeons',
+  },
+  fields: [
+    {
+      name: 'title',
+      label: 'Überschrift',
+      type: 'text',
+    },
+    {
+      name: 'text',
+      label: 'Text',
+      type: 'textarea',
+    },
+    {
+      name: 'accordions',
+      labels: {
+        singular: 'Eintrag',
+        plural: 'Einträge',
+      },
+      admin: {
+        initCollapsed: true,
+      },
+      type: 'array',
+      fields: [
+        {
+          name: 'title',
+          label: 'Titel',
+          type: 'text',
+          required: true,
+        },
+        {
+          name: 'text',
+          label: 'Text',
+          type: 'textarea',
+          required: true,
+        },
+      ],
+    },
+  ],
+}
