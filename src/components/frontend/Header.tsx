164bb349ea9eddd63adc36efc84c13163165a3cb
Nina Aßmann <hallo@ninaassmann.de>
2026-01-12 13:28:00 +0100

    add responsiveness to the header menu

--- src/components/frontend/Header.tsx
+++ src/components/frontend/Header.tsx
@@ -1,4 +1,5 @@
 import config from '@/payload.config'
+import { Menu } from 'lucide-react'
 import Link from 'next/link'
 
 export async function Header() {
@@ -9,12 +10,33 @@ export async function Header() {
       <Link href="/">
         <span className="font-serif text-2xl text-primary">Faszination Hund</span>
       </Link>
-      <div className="flex gap-10">
+      <nav className="hidden sm:flex gap-10 h-10 items-center">
         <Link href="/hunderassen">Hunderassen</Link>
         <Link href="/blog">Blog</Link>
         <Link href={payloadConfig.routes.admin} rel="noopener noreferrer" target="_blank">
           Login
         </Link>
+      </nav>
+      <div className="inline-block sm:hidden dropdown dropdown-end">
+        <button tabIndex={0} className="btn btn-square btn-ghost">
+          <Menu />
+        </button>
+        <ul
+          tabIndex="-1"
+          className="dropdown-content menu bg-base-100 rounded z-1 w-52 p-2 shadow-sm"
+        >
+          <li>
+            <Link href="/hunderassen">Hunderassen</Link>
+          </li>
+          <li>
+            <Link href="/blog">Blog</Link>
+          </li>
+          <li>
+            <Link href={payloadConfig.routes.admin} rel="noopener noreferrer" target="_blank">
+              Login
+            </Link>
+          </li>
+        </ul>
       </div>
     </header>
   )
