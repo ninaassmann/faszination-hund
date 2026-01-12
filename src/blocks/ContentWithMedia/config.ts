3209cb531487103e6a126803913845c309ea3567
Nina Aßmann <hallo@ninaassmann.de>
2026-01-12 14:02:42 +0100

    add defaultValues

--- src/blocks/ContentWithMedia/config.ts
+++ src/blocks/ContentWithMedia/config.ts
@@ -28,6 +28,7 @@ export const ContentWithMedia: Block = {
                 { label: 'H2', value: 'h2' },
                 { label: 'H3', value: 'h3' },
               ],
+              defaultValue: { label: 'H2', value: 'h2' },
               admin: {
                 description:
                   'Wähle die HTML-Überschriftsebene. H2 für Hauptabschnitte, H3 für Unterabschnitte.',
@@ -81,6 +82,7 @@ export const ContentWithMedia: Block = {
                 { label: 'Hell', value: 'var(--frontend-base-200)' },
                 { label: 'Dunkel', value: 'var(--frontend-neutral)' },
               ],
+              defaultValue: { label: 'Allgemein', value: 'var(--frontend-general)' },
             },
             {
               name: 'colorInfo',
@@ -101,6 +103,7 @@ export const ContentWithMedia: Block = {
                 { label: 'Links', value: 'left' },
                 { label: 'Rechts', value: 'right' },
               ],
+              defaultValue: { label: 'Links', value: 'left' },
             },
           ],
         },
