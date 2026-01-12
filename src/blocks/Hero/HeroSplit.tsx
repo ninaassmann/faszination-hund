627c894456794151aad1c7c2b48920f0d26c7428
Nina Aßmann <hallo@ninaassmann.de>
2026-01-12 13:43:07 +0100

    feat(hero): make HeroSplit component responsive

--- src/blocks/Hero/HeroSplit.tsx
+++ src/blocks/Hero/HeroSplit.tsx
@@ -25,17 +25,20 @@ export const HeroSplit: React.FC<Props> = ({
 
   return (
     <section className={`py-28 ${backgroundClass} ${className ?? ''}`}>
-      <div className="container flex justify-between items-center">
+      <div className="container flex flex-wrap justify-between items-center">
+        {/* Textposition Left */}
         {textPosition == 'left' && (
-          <div className="w-1/2">
+          <div className="w-full sm:w-2/3 lg:w-1/2">
             <span className="uppercase tracking-widest text-sm mb-2">{eyebrow}</span>
             <h1>{headline}</h1>
             <p className="max-w-[80%]">{text}</p>
             <HeroCtas ctas={ctas} />
           </div>
         )}
+
+        {/* Image */}
         {mediaImage?.url && (
-          <div className="rounded-2xl aspect-[2/3] overflow-clip relative w-1/3">
+          <div className="rounded-2xl aspect-[3/2] sm:aspect-[2/3] overflow-clip relative w-full sm:w-1/3">
             <Image
               src={mediaImage?.url}
               alt={mediaImage?.alt}
@@ -56,8 +59,9 @@ export const HeroSplit: React.FC<Props> = ({
           </div>
         )}
 
+        {/* Textposition Right */}
         {textPosition == 'right' && (
-          <div className="w-1/2">
+          <div className="w-full sm:w-2/3 lg:w-1/2 mt-6 sm:mt-0">
             <span className="uppercase tracking-widest text-sm mb-2">{eyebrow}</span>
             <h1>{headline}</h1>
             <p className="max-w-[80%]">{text}</p>
