a0054b6d533c518dc5c0620f456823ff0b4bc688
Nina Aßmann <hallo@ninaassmann.de>
2026-01-13 12:12:06 +0100

    chore(payload): add migration for current schema

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,5 +1,6 @@
 import * as migration_20251019_111216 from './20251019_111216';
 import * as migration_20260109_200024 from './20260109_200024';
+import * as migration_20260113_111125 from './20260113_111125';
 
 export const migrations = [
   {
@@ -10,6 +11,11 @@ export const migrations = [
   {
     up: migration_20260109_200024.up,
     down: migration_20260109_200024.down,
-    name: '20260109_200024'
+    name: '20260109_200024',
+  },
+  {
+    up: migration_20260113_111125.up,
+    down: migration_20260113_111125.down,
+    name: '20260113_111125'
   },
 ];
